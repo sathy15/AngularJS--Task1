134ac95f0d5cd5c4777ad4799930febb4a6057d9
sathy15 <99sathyamoorthi@gmail.com>
2022-03-01 09:44:22 +0530

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,11 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent {
   name1 = 'Angular' + VERSION.major;
   isshow = false;
+
+  myFunc() {
+    var num1 = (document.getElementById('exchageRateDate') as HTMLInputElement)
+      .value;
+    // console.log(num1);
+    alert("your selected place is" +"  "+num1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,6 @@ import { UserComponent } from '../user/user.component';
 @NgModule({
   imports:      [ BrowserModule, FormsModule,HttpClientModule ],
   declarations: [ AppComponent, HelloComponent ,ProjectComponent,UserComponent],
-  bootstrap:    [UserComponent]
+  bootstrap:    [AppComponent]
 })
 export class AppModule { }
